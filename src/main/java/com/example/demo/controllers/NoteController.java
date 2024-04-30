047e6a90d67d8320c0c8a2d73c06e452c31376d1
Roadway000 <roadway000@gmail.com>
2024-04-30 11:34:44 +0300

    Spring Boot MVC module 15

--- src/main/java/com/example/demo/controllers/NoteController.java
+++ src/main/java/com/example/demo/controllers/NoteController.java
@@ -18,14 +18,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class NoteController {
     @Autowired private NoteService noteService;
     @Autowired private NoteConvertor noteConvertor;
-//    @RequestMapping(value = "/list", method = {RequestMethod.GET})
     @GetMapping(value = "/list")
     public ModelAndView noteList() {
         ModelAndView result = new ModelAndView("notes/listNotes");
         result.addObject("notes", noteConvertor.dtosToResponses(noteService.listAll()));
         return result;
     }
-//    @RequestMapping(value = "/create", method = {RequestMethod.POST})
     @PostMapping(value = "/create")
     public ModelAndView createNote(
             @RequestParam(value="title") /** @Size(min = 1, max = 250) */ String title,
@@ -36,21 +34,17 @@ public class NoteController {
         noteService.add(dto);
         return noteList();
     }
-//    @DeleteMapping("/delete")
-//    @RequestMapping(value = "/delete", method = {RequestMethod.POST})
     @PostMapping(value = "/delete")
     public ModelAndView deleteNoteById(@Valid @NotNull @RequestParam(value="id") long id) {
         noteService.deleteById(id);
         return noteList();
     }
-//    @RequestMapping(value = "/edit", method = {RequestMethod.GET})
     @GetMapping(value = "/edit")
     public ModelAndView getNoteForEdit(@NotNull @RequestParam(value="id") long id) {
         ModelAndView result = new ModelAndView("notes/editNote");
         result.addObject("note", noteConvertor.dtoToResponse(noteService.getById(id)));
         return result;
     }
-//    @RequestMapping(value = "/update", method = {RequestMethod.POST})
     @PostMapping(value = "/update")
     public ModelAndView updateNote(
             @NotNull @RequestParam(value="id") int id,
