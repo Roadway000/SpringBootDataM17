3e0e73c5c8a333be6e2ab37e8b52b27754625395
Roadway000 <roadway000@gmail.com>
2024-04-30 11:10:54 +0300

    Spring Boot MVC module 15

--- src/main/java/com/example/demo/service/NoteService.java
+++ src/main/java/com/example/demo/service/NoteService.java
@@ -1,14 +1,14 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Note;
+import com.example.demo.data.dto.NoteDto;
 
 import java.util.List;
 
 public interface NoteService {
-    List<Note> listAll();
-    Note add(Note note);
+    List<NoteDto> listAll();
+    NoteDto add(NoteDto note);
     void deleteById(Long id);
     void deleteLast();
-    void update(Note note);
-    Note getById(Long id);
+    void update(NoteDto note);
+    NoteDto getById(Long id);
 }
